3c14da2d629949f7fa7e4ff34185ff462f740bbe
Jason Chen <71620462+JasonChen1203@users.noreply.github.com>
2024-01-09 23:08:27 -0500

    Update sort python dict by key or value.py

--- Notes/sort python dict by key or value.py
+++ Notes/sort python dict by key or value.py
@@ -1,10 +1,10 @@
 # sort python dict by key
-dict = {3: 10, 2: 9, 1: 15, 15: 2, 10: 32}
-sorted_keys = sort(list(dict.keys()))
-sorted_dict_by_key = {i:dict[i] for i in sorted_keys}
+old_dict = {3: 10, 2: 9, 1: 15, 15: 2, 10: 32}
+sorted_keys = sort(list(old_dict.keys()))
+sorted_dict_by_key = {i:old_dict[i] for i in sorted_keys}
 print(sorted_dict_by_key)
 
 # sort python dict by value
-dict = {3: 10, 2: 9, 1: 15, 15: 2, 10: 32}
-sorted_dict_by_value = dict(sorted(dict.items(), key = lambda item: item[1]))
+old_dict = {3: 10, 2: 9, 1: 15, 15: 2, 10: 32}
+sorted_dict_by_value = dict(sorted(old_dict.items(), key = lambda item: item[1]))
 print(sorted_dict_by_value)
