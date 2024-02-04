dc665ef79785250a203f65d8dfdf7b3cc84e2d72
Jason Chen <71620462+abc1203@users.noreply.github.com>
2024-02-04 00:32:29 -0500

    Create Strings.cpp

---
+++ Notes/C++/Strings.cpp
@@ -0,0 +1,13 @@
+// Substrings
+string s = "abcde";
+cout << s.subtr(2, 2) << endl; // starts from index 2 & copies 2 chars
+
+// Concat
+string first = "jason";
+string last = "chen";
+string full = first + last;
+cout << full << endl;
+
+// To convert from string to int, run stoi(s)
+// can get char by calling s[i]
+// s.length(), s.size()
